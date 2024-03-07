30dc0130074b7dd8766b944b00f2e14687693cfb
franciscomateusrodriguescastro <matesrodrigues@gmail.com>
2024-03-07 18:55:43 -0300

    teste

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-document.getElementById("cadastroForm").addEventListener("submit", function(event) {
-    var nome = document.getElementById("nome").value;
-    var cpf = document.getElementById("cpf").value;
-    var email = document.getElementById("email").value;
+const formulario = document.queruSelector("fora");
+const Inome = document.querySelector("nome");
+const Icpf = document.querySelector("cpf");
+const Iemail = document.querySelector("email");
+const Itel = document.querySelector("tel");
 
+function cadastrar () {
     
-    if (nome.trim() === "" || cpf.trim() === "" || email.trim() === "") {
-        alert("Por favor, preencha todos os campos obrigatórios.");
-        event.preventDefault(); 
-    }
+     fetch("echo/json/"), 
+     {
+        headers: {
+            'Accept': 'application/json',
+            'Content=Type': 'application/jason'
+        },
+        methon: "POST",
+        body: JSON.stringify({ a: 1, b: 2 })
+     })
+     .then(function (res) {console.log(res) })
+     .catch(function (res) {console.log(res) })
+    };
+
+
+formulario.addEventListener('submit', (event) {
+        event.preventDefault();
+
+        cont dados = {
+            nome: Inome.value
+            nome: Icpf.value
+            nome: Iemail.value
+            nome: Itel.value
+
+};
 });
